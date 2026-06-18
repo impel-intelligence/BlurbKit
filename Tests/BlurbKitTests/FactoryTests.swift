e93daf5d2f21e19e797905484f82e888f019fa18
Taylor Lineman <git@actuallytaylor.com>
2026-06-18 16:43:05 -0400

    Added a test to ensure the factory intakes the correct types
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

---
+++ Tests/BlurbKitTests/FactoryTests.swift
@@ -0,0 +1,33 @@
+//
+//  ProtocolTests.swift
+//  IrisSearch
+//
+//  Created by Taylor Lineman on 6/17/26.
+//
+
+import Testing
+@testable import BlurbKit
+import Foundation
+import UniformTypeIdentifiers
+
+struct FactoryTests {
+    @Test("Ensure the text digester is returned by the factory is correct for various text UTTypes.", arguments: [
+        UTType.plainText,
+        UTType.cHeader,
+        UTType.cSource,
+        UTType.swiftSource,
+        UTType("com.unknown.md")!
+    ]) func testTxtDigesterReturned(utType: UTType) throws {
+        let returnedDigester = try BlurbFactory.provider(for: utType)
+        #expect(type(of: TextBlurbProvider()) == type(of: returnedDigester))
+    }
+    
+    
+    @Test("Ensure the pdf digester is returned by the factory is correct for various pdf UTTypes.", arguments: [
+        UTType.pdf,
+        UTType("com.adobe.pdf")!,
+    ]) func testPDFDigesterReturned(utType: UTType) throws {
+        let returnedDigester = try BlurbFactory.provider(for: utType)
+        #expect(type(of: PDFBlurbProvider()) == type(of: returnedDigester))
+    }
+}
